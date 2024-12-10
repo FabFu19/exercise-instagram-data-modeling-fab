885786b28d3067f95f370921a5429292626204ab
Fabian Fuenmayor <fabianfuen19@icloud.com>
2024-12-10 16:14:10 +0000

    feat(instagram data model): completed and location model added

--- src/models.py
+++ src/models.py
@@ -7,6 +7,14 @@ from eralchemy2 import render_er
 
 Base = declarative_base()
 
+class Followers(Base):
+    __tablename__ = 'followers'
+    id = Column(Integer, primary_key = True)
+    from_id = Column(Integer, ForeignKey('users.id'))
+    from_user = Column(Integer, ForeignKey('users.id'))
+    to_id = relationship('Users', foreign_keys=[from_id], backref= 'follower')
+    to_user = relationship('Users', foreign_keys=[from_id], backref= 'following')
+
 class Users(Base):
     __tablename__ = 'users'
     id = Column(Integer, primary_key = True)
@@ -14,6 +22,8 @@ class Users(Base):
     email = Column(String, nullable = False, unique = True)
     password = Column(String, nullable = False)
     born_date = Column(Integer, nullable = False)
+    location_id = Column(Integer, ForeignKey('location.id'))
+    location = relationship('Location', backref= 'users')
 
     def to_dict(self):
         return {
@@ -30,7 +40,9 @@ class Posts(Base):
      title = Column(String, nullable = False)
      content = Column(String, nullable = False)
      user_id = Column(Integer, ForeignKey('users.id'))
-     user = relationship('Users', backref = 'posts' )
+     user = relationship('Users', backref = 'posts')
+     location_id = Column(Integer, ForeignKey('location.id'))
+     location = relationship('Location', backref= 'posts')
 
      def to_dict(self):
          return {
@@ -44,7 +56,9 @@ class Comments(Base):
      id = Column(Integer, primary_key = True)
      content = Column(String, nullable = False)
      user_id = Column(Integer, ForeignKey('users.id'))
-     user = relationship('Users', backref = 'comments' )
+     user = relationship('Users', backref = 'comments')
+     post_id = Column(Integer, ForeignKey('posts.id'))
+     post = relationship('Posts', backref = 'comments' )
 
      def to_dict(self):
          return {
@@ -63,9 +77,19 @@ class Medias(Base):
              "id": self.id,
              "src": self.src
          }
-    
+     
+class Location(Base):
+    __tablename__ = 'location'
+    id = Column(Integer, primary_key = True)
+    name = Column(String, nullable = False)
+    address = Column(String, nullable = True) 
 
-    
+    def to_dict(self):
+         return {
+             "id": self.id,
+             "src": self.name,
+             "address": self.address
+         }
 
 ## Draw from SQLAlchemy base
 try:
